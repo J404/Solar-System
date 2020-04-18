5f0750add874ad8fd2f7d79ad435eb55aa51b198
Jimmy <jimmystry@gmail.com>
2020-04-18 16:10:52 -0400

    Created & tested initial code for celestial bodies

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+let planet;
+
 function setup() {
     createCanvas(400, 400);
+
+    planet = new CelestialBody(width / 2, height / 2, 500, 20, 0, 0, 0);
 }
 
 function draw() {
     background(20);
+    
+    planet.update();
+    planet.show();
 }
