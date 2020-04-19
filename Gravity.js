bae52be7c07e2498aa9f3f67277117e77f03827d
Jimmy <jimmystry@gmail.com>
2020-04-19 19:47:15 -0400

    Can simulate path of a body correctly.

--- Gravity.js
+++ Gravity.js
@@ -10,7 +10,7 @@ const G = 6.674 * Math.pow(10, -11);
  */
 const calculateGravity = (bodyA, bodyB) => {
     const dist = p5.Vector.dist(bodyA.pos, bodyB.pos);
-    const forceMag = (G * bodyA.mass * bodyB.mass) / Math.pow(dist * 1000000, 2);
+    const forceMag = (G * bodyA.mass * bodyB.mass) / Math.pow(dist * Math.pow(10, 6), 2);
 
     const diffVec = p5.Vector.sub(bodyB.pos, bodyA.pos);
 
@@ -18,4 +18,20 @@ const calculateGravity = (bodyA, bodyB) => {
     force.setMag(forceMag);
 
     return force;
+}
+
+/**
+ * Calculates the gravitational force between two bodies based on their theoretical positions
+ * @param {CelestialBody} bodyA 
+ * @param {CelestialBody} bodyB 
+ * @return A p5.Vector containing the magnitude of the gravitational force experienced by both bodies, 
+ * and the direction of force experienced by bodyA
+ */
+const calculateGravityTheoretical = (bodyA, bodyB) => {
+    newBodyA = new CelestialBody(bodyA.theoreticalPos.x, bodyA.theoreticalPos.y,
+        bodyA.mass, bodyA.size, bodyA.theoreticalVel.x, bodyA.theoreticalVel.y, bodyA.color, bodyA.stationary);
+    newBodyB = new CelestialBody(bodyB.theoreticalPos.x, bodyB.theoreticalPos.y,
+        bodyB.mass, bodyB.size, bodyB.theoreticalVel.x, bodyB.theoreticalVel.y, bodyB.color, bodyB.stationary);
+
+    return calculateGravity(newBodyA, newBodyB);
 }
