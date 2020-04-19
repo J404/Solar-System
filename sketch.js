16f963a6d815f1c008648da5bedf3c7097c12e6a
Jimmy <jimmystry@gmail.com>
2020-04-19 14:57:57 -0400

    Can now achieve orbit, but still having trouble with moons. I'm goint to merge with master and try to find a way to simulate paths.

--- sketch.js
+++ sketch.js
@@ -1,23 +1,54 @@
 const celestialBodies = [];
+let startButton, stopButton;
+
+let running = false;
 
 function setup() {
     createCanvas(1000, 700);
 
-    celestialBodies.push(new CelestialBody((width / 2) - 100, (height / 2) - 50, (5.972 * Math.pow(10, 24)), 20, 0, 80, false));
-    celestialBodies.push(new CelestialBody((width / 2) + 100, (height / 2)- 50, (1.989 * Math.pow(10, 28)), 20, 0, 0, 0, false));
+    startButton = createButton("Start");
+    startButton.position(10, 10);
+    startButton.mousePressed(() => {
+        running = true;
+    });
+
+    stopButton = createButton("Stop");
+    stopButton.position(60, 10);
+    stopButton.mousePressed(() => {
+        running = false;
+    });
+
+    celestialBodies.push(new CelestialBody((width / 2) - 150, (height / 2) - 50, (5.972 * Math.pow(10, 23)), 20, 0, 25, false));
+    celestialBodies.push(new CelestialBody((width / 2) - 165, (height / 2) - 50, (1.12 * Math.pow(10, 8)), 5, 0, 26.75, false));
+    celestialBodies.push(new CelestialBody((width / 2) + 100, (height / 2)- 50, (1.989 * Math.pow(10, 27)), 20, 0, 0, 0, false));
     //celestialBodies.push(new CelestialBody((width / 2) - 175, height / 2, 52700000000, 20, 0, 0, 0));
 }
 
 function draw() {
     background(20);
-    frameRate(20);
+    //frameRate(5);
+    //noLoop();
 
-    for (let body of celestialBodies) {
-        body.update();
-        body.show();
+    if (running) {
+        for (let body of celestialBodies) {
+            body.update();
+        }
     }
 
+    for (let body of celestialBodies) {
+        body.show();
+    }
+    
     stroke("#FF0000");
     //line(celestialBodies[0].pos.x, celestialBodies[0].pos.y, 
     //    celestialBodies[0].pos.x + netForce.x, celestialBodies[0].pos.y + netForce.y);
+}
+
+function keyPressed() {
+    if (key == " ") {
+        for (let body of celestialBodies) {
+            body.update();
+            body.show();
+        }
+    }
 }
