ed4a28c13a905809c493d9369a8f493af2914b82
Jimmy <jimmystry@gmail.com>
2020-04-18 20:57:57 -0400

    Created first test of gravity. Can now get the gravitational attraction between two bodies.

---
+++ Gravity.js
@@ -0,0 +1,20 @@
+const G = 6.674 * Math.pow(10, -11);
+
+/**
+ * Calculates the force of attraction due to gravity between two bodies
+ * @param {CelestialBody} bodyA The first CelestialBody
+ * @param {CelestialBody} bodyB The second CelestialBody
+ * @return A p5.Vector containing the magnitude of the gravitational force experienced by both bodies, 
+ * and the direction of force experienced by bodyA
+ */
+const calculateGravity = (bodyA, bodyB) => {
+    const dist = p5.Vector.dist(bodyA.pos, bodyB.pos);
+    const forceMag = (G * bodyA.mass * bodyB.mass) / Math.pow(dist, 2);
+
+    const diffVec = p5.Vector.sub(bodyB.pos, bodyA.pos);
+
+    const force = p5.Vector.fromAngle(diffVec.heading());
+    force.setMag(forceMag);
+
+    return force;
+}
--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
 let planet;
+let planet2;
 
 function setup() {
     createCanvas(400, 400);
 
-    planet = new CelestialBody(width / 2, height / 2, 500, 20, 0, 0, 0);
+    planet = new CelestialBody(width / 2, height / 2, 52700000000, 20, 0, 0, 0);
+
+    planet2 = new CelestialBody((width / 2) + 100, height / 2, 52700000000, 20, 0, 0, 0);
 }
 
 function draw() {
     background(20);
     
     planet.update();
+    planet2.update();
+
     planet.show();
+    planet2.show();
+
+    let gravity = calculateGravity(planet, planet2);
+    
+    stroke("#FF0000");
+    line(planet.pos.x, planet.pos.y, planet.pos.x + gravity.x, planet.pos.y + gravity.y)
 }
