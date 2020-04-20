00e446af8be3c0510c7b7a060229c8d1a511b003
Jimmy <jimmystry@gmail.com>
2020-04-19 20:07:10 -0400

    I have been able to create a moon w/ a sustained orbit around a planet moving in one direction. Will now add try adding a sun.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 const celestialBodies = [];
+let planet, moon, sun;
+
 let startButton, stopButton;
 
 let sliders = [];
@@ -14,13 +16,16 @@ function setup() {
         running = true;
     });
 
-    celestialBodies.push(new CelestialBody((width / 2) - 170, (height / 2) - 50, (5.972 * Math.pow(10, 20)), 5, 0, 9.75, 0, false)); // planet
-    celestialBodies.push(new CelestialBody((width / 2) - 170.003, (height / 2) - 50, (7.3 * Math.pow(10, 16)), 2, 0, 9.7, 0, false)); // moon
-    celestialBodies.push(new CelestialBody((width / 2), (height / 2)- 50, (1.989 * Math.pow(10, 26)), 20, 0, 0, 0, false)); // sun
-    celestialBodies.push(new CelestialBody((width / 2) - 175, height / 2, 52700000000, 20, 0, 0, 0));
+    //sun = new CelestialBody(width / 2, height / 2, 2 * Math.pow(10, 26), 30, 0, 0, 0, false);
+    planet = new CelestialBody((width / 2) - 170, height / 2, 1.5 * Math.pow(10, 24), 10, 0, 2, 0, false);
+    moon = new CelestialBody((width / 2) - 185, height / 2, 1.123 * Math.pow(10, 20), 1, 0, 4.75, 0, false);
+
+    //celestialBodies.push(sun);
+    celestialBodies.push(planet);
+    celestialBodies.push(moon);
 
     for (let i = 0; i < celestialBodies.length; i++) {
-        const slider = createSlider(0, 75, celestialBodies[i].vel.y, 0.25);
+        const slider = createSlider(0, 50, celestialBodies[i].vel.y, 0.25);
         slider.position(10, (i * 30) + 30);
         slider.value(celestialBodies[i].vel.y);
         sliders.push(slider);
